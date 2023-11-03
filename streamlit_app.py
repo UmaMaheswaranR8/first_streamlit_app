593a9ef88e2b18b1019702bbe6bd3d19d1040d18
UmaMaheswaranR8 <149758371+UmaMaheswaranR8@users.noreply.github.com>
2023-11-03 11:50:11 +0530

    Update streamlit_app.py
    smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title ('My Parents Healthy Diner')
 streamlit.header ('Menu')
 streamlit.text ('🥣 Idly')
 streamlit.text (' 🥗 Dosa')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
