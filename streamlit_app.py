8d93bd7764912682d3527346b91651aabad90388
UmaMaheswaranR8 <149758371+UmaMaheswaranR8@users.noreply.github.com>
2023-11-03 11:49:43 +0530

    Update streamlit_app.py
    adding images

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title ('My Parents Healthy Diner')
 
 streamlit.header ('Menu')
-streamlit.text ('Idly')
-streamlit.text ('Dosa')
+streamlit.text ('🥣 Idly')
+streamlit.text (' 🥗 Dosa')
