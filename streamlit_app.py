50607c45b92015255db3dad200454fd82d4ca387
UmaMaheswaranR8 <149758371+UmaMaheswaranR8@users.noreply.github.com>
2023-11-03 11:54:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit
 
-streamlit.title ('My Parents Healthy Diner')
-
-streamlit.header ('Menu')
-streamlit.text ('🥣 Idly')
-streamlit.text (' 🥗 Dosa')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
